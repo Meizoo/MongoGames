35286509348817bd5b8dad72ff92c3e21bd72d78
oozam <dawidek199710@wp.pl>
2020-06-15 12:49:53 +0200

    Add project files.

---
+++ MongoDBGames/Model/GameContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace MongoDBGames.Model
+{
+    public class GameContext : IGameContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public GameContext(IOptions<Settings> options, IMongoClient client)
+        {
+            _db = client.GetDatabase(options.Value.Database);
+        }
+
+        public IMongoCollection<Game> Games => _db.GetCollection<Game>("Games");
+    }
+}
---
+++ MongoDBGames/Repository/IGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDBGames.Model;
+
+namespace MongoDBGames.Repository
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> GetAllGames();
+        Task<Game> GetGame(string name);
+        Task Create(Game game);
+        Task<bool> Update(Game game);
+        Task<bool> Delete(string name);
+    }
+}
